0de9cfd001596f295b80c15632b70000a4a91bd0
Yolserve Ngoma <yolserven@gmail.com>
2022-12-03 10:09:22 +0100

    Modiffication du nombre d'articles à afficher, creation du fichier de migration

--- src/Controller/NewsController.php
+++ src/Controller/NewsController.php
@@ -31,7 +31,7 @@ class NewsController extends AbstractController
 
         return $this->render('news/index.html.twig', [
             'articles' => $articles,
-            'latest_articles' => $this->articleRepository->findBy([], ['createdAt' => 'ASC'], 6),
+            'latest_articles' => $this->articleRepository->findBy([], ['createdAt' => 'ASC'], 5),
             'form' => $form->createView()
         ]);
     }
@@ -43,11 +43,11 @@ class NewsController extends AbstractController
         $form = $this->createForm(SearchNewsType::class, $dataSearch);
         $form->handleRequest($request);
         $data = $this->articleRepository->findBysearch($dataSearch);
-        $articles = $paginator->paginate($data, $request->query->getInt('page', 1),8 );
+        $articles = $paginator->paginate($data, $request->query->getInt('page', 1),6 );
 
         return $this->render('news/search_result.html.twig', [
             'articles' => $articles,
-            'latest_articles' => $this->articleRepository->findBy([], ['createdAt' => 'ASC'], 6),
+            'latest_articles' => $this->articleRepository->findBy([], ['createdAt' => 'ASC'], 5),
             'form' => $form->createView()
         ]);
     }
@@ -60,7 +60,7 @@ class NewsController extends AbstractController
 
         return $this->render('news/show.html.twig', [
             'article' => $article,
-            'latest_articles' => $this->articleRepository->findBy([], ['createdAt' => 'ASC'], 6),
+            'latest_articles' => $this->articleRepository->findBy([], ['createdAt' => 'ASC'], 5),
             'form' => $form->createView()
         ]);
     }
