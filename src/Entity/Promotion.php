53a24eeed336f9ada543f354c91e74b8bbe255ca
Yolserve Ngoma <yolserven@gmail.com>
2022-12-06 18:23:15 +0100

    Creation des entités Promotion et Alumni, creation du controlleur Alumni, creation des fichiers de migration

---
+++ src/Entity/Promotion.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PromotionRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PromotionRepository::class)]
+class Promotion
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $label = null;
+
+    #[ORM\ManyToOne(inversedBy: 'promotion')]
+    private ?Alumni $alumnies = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLabel(): ?string
+    {
+        return $this->label;
+    }
+
+    public function setLabel(string $label): self
+    {
+        $this->label = $label;
+
+        return $this;
+    }
+
+    public function getAlumnies(): ?Alumni
+    {
+        return $this->alumnies;
+    }
+
+    public function setAlumnies(?Alumni $alumnies): self
+    {
+        $this->alumnies = $alumnies;
+
+        return $this;
+    }
+}
