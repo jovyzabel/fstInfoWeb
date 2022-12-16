556d569147c91247e8cf15dbaa24dbf8951f02e2
jovyzabel <jovymonka7@gmail.com>
2022-12-16 16:48:59 +0100

    creation of multi steps form

--- src/Form/StudentType.php
+++ src/Form/StudentType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\Student;
+use Doctrine\DBAL\Types\StringType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints\File;
@@ -10,15 +11,21 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class StudentType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('firstName')
+            ->add('name', TextType::class, [
+                "label" => "Nom"
+            ])
+            ->add('firstName' , TextType::class, [
+                "label" => "Prénom"
+            ])
             ->add('civility', ChoiceType::class, [
+                'label'=> 'Civilité',
                 'choices' => [
                     'Monsieur' => 1,
                     'Madame' => 2,
@@ -26,9 +33,12 @@ class StudentType extends AbstractType
                 
             ])
             ->add('birthDay',DateType::class, [
+                "label" => "Date de Naissance",
                 "widget" => "single_text"
             ])
-            ->add('birthPlace')
+            ->add('birthPlace', TextType::class, [
+                "label" => "Lieu de naissance"
+            ])
             ->add('telephone')  
             ->add('email')
             ->add('address', AddressType::class)
