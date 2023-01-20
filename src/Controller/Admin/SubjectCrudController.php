4c503ef6f94e714ed59b5f46afa74f9c9b86c919
Yolserve Ngoma <yolserven@gmail.com>
2023-01-20 23:08:25 +0100

    Amelioration

--- src/Controller/Admin/SubjectCrudController.php
+++ src/Controller/Admin/SubjectCrudController.php
@@ -5,9 +5,10 @@ namespace App\Controller\Admin;
 use App\Entity\Subject;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 
 class SubjectCrudController extends AbstractCrudController
 {
@@ -21,6 +22,7 @@ class SubjectCrudController extends AbstractCrudController
         return [
             IdField::new('id')->onlyOnIndex(),
             TextField::new('label'),
+            NumberField::new('credits'),
             TextareaField::new('description'),
             AssociationField::new('ue'),
             AssociationField::new('teacher')
